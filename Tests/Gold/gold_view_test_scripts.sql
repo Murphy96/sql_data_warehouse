ab05858d364a101b88eb557375610bcec4d5de87
Murphy Bovbjerg <murphybovbjerg@gmail.com>
2026-06-16 10:21:14 -0400

    Update gold_view_test_scripts.sql

--- Tests/Gold/gold_view_test_scripts.sql
+++ Tests/Gold/gold_view_test_scripts.sql
@@ -1,3 +1,27 @@
+/*
+*******************************************************************************************
+Quality Check on DDL for Gold Layer
+*******************************************************************************************
+
+Script Purpose: 
+	Performs quality checks for DDL integrity in the gold layer after joins and enrichment. 
+It includes checks for: 
+		- Column Names
+		- Data Types
+        - Max length
+        - Nullibility 
+Usage Notes: 
+	- Run after creating the gold layer view
+	- Resolve any discrepancies found during check based on expected results 
+      this is the last check for data types found in the business ready data, 
+      ensure accuracy. 
+*/
+
+/*
+**************************************************************
+Checks DDL for View gold.dim_customers
+**************************************************************
+*/
 SELECT
     c.column_id,
     c.name AS column_name,
@@ -10,6 +34,12 @@ JOIN sys.types t
 WHERE c.object_id = OBJECT_ID('DataWarehouse.gold.dim_customers')
 ORDER BY c.column_id;
 
+/*
+**************************************************************
+Checks DDL for View gold.dim_products
+**************************************************************
+*/
+
 SELECT
     c.column_id,
     c.name AS column_name,
@@ -21,6 +51,12 @@ JOIN sys.types t
     ON c.user_type_id = t.user_type_id
 WHERE c.object_id = OBJECT_ID('DataWarehouse.gold.dim_products')
 ORDER BY c.column_id;
+
+/*
+**************************************************************
+Checks DDL for View gold.fact_sales
+**************************************************************
+*/
 
 SELECT
     c.column_id,
