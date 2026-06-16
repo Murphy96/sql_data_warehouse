e7ebcfac0d7a933200f9a30024d10d61848e93b6
Murphy Bovbjerg <murphybovbjerg@gmail.com>
2026-06-16 10:15:59 -0400

    Update gold_test_scripts.sql

--- Tests/Gold/gold_test_scripts.sql
+++ Tests/Gold/gold_test_scripts.sql
@@ -1,41 +1,55 @@
---  Check information in duplicate gender column 
---In cases of data discrepancies, the crm source system is more accurate 
-SELECT DISTINCT
+/*
+*******************************************************************************************
+Quality Checks
+*******************************************************************************************
 
-	ci.cst_gndr, 
-	ca.gen, 
-	CASE WHEN ci.cst_gndr != 'Unknown' THEN ci.cst_gndr
-	     ELSE COALESCE( ca. gen, 'Unknown')
-	END AS new_gen
-
-FROM silver.crm_cust_info ci 
-
-LEFT JOIN silver.erp_cust_az12 ca
-ON		  ci.cst_key = ca.cid 
-LEFT JOIN silver.erp_loc_a101 la
-ON		  ci.cst_key = la.cid
-ORDER BY 1,2
+Script Purpose: 
+	Performs quality checks for key integrity in the gold layer after joins and enrichment. 
+It includes checks for: 
+		- Null or duplicate primary keys
+		- Checks for information discrepenancies between systems. 
+Usage Notes: 
+	- Run after creating the gold layer view
+	- Resolve any discrepancies found during check based on expected results 
+*/
 
 
-
+/*
+*******************************************************
+Check on gold.dim_customers view for duplication errors
+*******************************************************
+*/
+-- Check information in duplicate gender column 
+--Expectation: No conflicting information between tables
 SELECT * FROM gold.dim_customers
 
 SELECT distinct gender FROM gold.dim_customers
+/*
+************************************************
+Quick check to ensure column order, data
+************************************************
+*/
+SELECT * FROM gold.dim_customers	
 
 SELECT * FROM gold.dim_products
 
 SELECT * FROM gold.fact_sales
 
---Foreign Key Integrity (Dimensions)
+/*
+************************************************
+Foreign Key Integrity (Dimensions)
+************************************************
+*/
+--gold.dim_customer key integrity 
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+WHERE c.customer_key IS NULL 	
+
+	--gold.dim_products key integrity 
 SELECT *
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_products p
 ON p.product_key = f.product_key
 WHERE p.product_key IS NULL 
-
-
-SELECT *
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
-WHERE c.customer_key IS NULL 
